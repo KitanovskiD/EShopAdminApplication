f7c3c67f20a7634dc6780ca3fd86d16e22d42c29
KitanovskiD <dimitar.kitanovski@outlook.com>
2021-04-21 20:55:19 +0200

    Initial Commit

---
+++ EShopAdminApplication/Controllers/OrderController.cs
@@ -0,0 +1,52 @@
+﻿using EShopAdminApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EShopAdminApplication.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IActionResult Index()
+        {
+            HttpClient client = new HttpClient();
+
+
+            string URI = "https://localhost:44309/api/Admin/GetOrders";
+
+            HttpResponseMessage responseMessage = client.GetAsync(URI).Result;
+
+            var result = responseMessage.Content.ReadAsAsync<List<Order>>().Result;
+
+            return View(result);
+        }
+
+        public IActionResult Details(Guid id)
+        {
+            HttpClient client = new HttpClient();
+
+
+            string URI = "https://localhost:44309/api/Admin/GetDetailsForProduct";
+
+            var model = new
+            {
+                Id = id
+            };
+
+            HttpContent content = new StringContent(JsonConvert.SerializeObject(model), Encoding.UTF8, "application/json");
+
+            HttpResponseMessage responseMessage = client.PostAsync(URI, content).Result;
+
+
+            var result = responseMessage.Content.ReadAsAsync<Order>().Result;
+
+
+            return View(result);
+        }
+    }
+}
